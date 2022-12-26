9515412436d7dd32e7db71cfc323e4eacdaa780c
Nikolay <labazopelastra@gmail.com>
2022-12-26 17:36:29 +0700

    add exam67

---
+++ Exam67/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+System.Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumNumbers(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+
+    return (num % 10 + SumNumbers(num / 10));
+}
+
+System.Console.WriteLine(SumNumbers(N));
+
